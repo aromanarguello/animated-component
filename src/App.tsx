488a37d6f73a190bf943d6895aca5741919d2fb2
Alejandro <aromanarguello@gmail.com>
2019-06-23 17:25:08 -0400

    fix types

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
-import * as React from "react";
-import { Card, FormInput, Notification } from "./components";
-import { useSpring, animated } from "react-spring";
-import styled from "styled-components";
+import * as React from "react"
+import { Card, FormInput, Notification } from "./components"
+import { useSpring, animated } from "react-spring"
+import styled from "styled-components"
 
 const Layout = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
-`;
+`
 
-const Header = styled.h1``;
+const Header = styled.h1``
 
-const noop = () => {};
+const noop = () => {}
 
 const App: React.FC<any> = () => {
-  const [inputValue, setInputValue] = React.useState("");
-  const [isActive, setIsActive] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState("")
+  const [isActive, setIsActive] = React.useState(false)
 
   const [props, set] = useSpring(() => ({
     opacity: 1,
     color: inputValue === "" ? "red" : "green",
     config: {
       tension: 350,
-      friction: 200
+      friction: 300
     }
-  }));
+  }))
 
   set({
-    opacity: isActive ? 1 : 0,
-    color: inputValue === "" ? "red" : "green"
-  });
+    opacity: isActive ? 1 : 0
+  })
 
   return (
     <Layout>
@@ -47,7 +46,7 @@ const App: React.FC<any> = () => {
         <Notification msg={inputValue === "" ? "Enter Text" : "Success"} />
       </animated.div>
     </Layout>
-  );
-};
+  )
+}
 
-export default App;
+export default App
